50f4a66700400a480ffc17e97f68504f298f258f
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2024-12-12 04:49:25 -0400

    Se agregaron funciones nuevas  a los dos nuevos botones.

---
+++ index.js
@@ -0,0 +1,12 @@
+function Contact_Dev(){
+   let Section_Developers = document.getElementById("Section_Developers");
+   Section_Developers.style.display ="flex";
+   let main_section = document.getElementById("main_section");
+   main_section.style.display ="none";
+}
+function Contact_Company(){
+    let Section_Developers = document.getElementById("Section_Developers");
+   Section_Developers.style.display ="none";
+   let main_section = document.getElementById("main_section");
+   main_section.style.display ="flex";
+}
