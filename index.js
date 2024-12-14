76ddb743cb04e981f014207fcd818449c211c31f
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2024-12-14 19:07:52 -0400

    Se añadieron las funciones de activación para los distintos formularios de contacto de los desarrolladores.

--- index.js
+++ index.js
@@ -15,11 +15,62 @@ function Contact_Company(){
    Section_Developers.style.display ="none";
    let main_section = document.getElementById("main_section");
    main_section.style.display ="flex";
-   let Contact_Dev = document.getElementById("Contact_Dev")
-   Contact_Dev.style.opacity ="0.7"
-   Contact_Dev.style.border = "none"
-   let Contact_Company = document.getElementById("Contact_Company")
-   Contact_Company.style.opacity = "1"
-   Contact_Company.style.borderBottom = "1px solid white"
+   let Contact_Dev = document.getElementById("Contact_Dev");
+   Contact_Dev.style.opacity ="0.7";
+   Contact_Dev.style.border = "none";
+   let Contact_Company = document.getElementById("Contact_Company");
+   Contact_Company.style.opacity = "1";
+   Contact_Company.style.borderBottom = "1px solid white";
+   let Urpirio = document.getElementById("Urpirio");
+   Urpirio.style.display = "none";
+   Urpirio.style.animation ="Pop-Up 1s"
+   let Yohandy = document.getElementById("Yohandy");
+   Yohandy.style.display = "none";
+   let Nashla = document.getElementById("Nashla");
+   Nashla.style.display = "none";
+   let Dioveny =document.getElementById("Dioveny")
+   Dioveny.style.display = "none"
+   let Nelson = document.getElementById("Nelson")
+   Nelson.style.display ="none";
+   let batuce = document.getElementById("batuce");
+   batuce.style.display = "none";
    
+}
+function Urpirio(){
+   let Urpirio = document.getElementById("Urpirio");
+   Urpirio.style.display = "flex";
+}
+function Salir(){
+   let Urpirio = document.getElementById("Urpirio");
+   Urpirio.style.display = "none";
+   let Yohandy = document.getElementById("Yohandy");
+   Yohandy.style.display = "none"
+   let Nashla = document.getElementById("Nashla");
+   Nashla.style.display = "none";
+   let Dioveny =document.getElementById("Dioveny")
+   Dioveny.style.display = "none";
+   let Nelson = document.getElementById("Nelson")
+   Nelson.style.display ="none";
+   let batuce = document.getElementById("enmanuel");
+   batuce.style.display = "none";
+}
+function Yohandy(){
+   let Yohandy = document.getElementById("Yohandy");
+   Yohandy.style.display = "Flex";
+}
+function Nashla(){
+   let Nashla = document.getElementById("Nashla");
+   Nashla.style.display = "flex";
+}
+function Dioveny(){
+   let Dioveny =document.getElementById("Dioveny");
+   Dioveny.style.display = "flex";
+}
+function Nelson(){
+   let Nelson = document.getElementById("Nelson");
+   Nelson.style.display ="flex";
+}
+function enmanuel(){
+   let batuce = document.getElementById("enmanuel");
+   batuce.style.display = "flex";
 }
